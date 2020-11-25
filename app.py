b38c8c7920fe7b244c5e30fb3f6b2d4a2e97b728
Quantum404 <sarah27166@gmail.com>
2020-11-25 12:10:42 +0530

    Added runApp function

--- app.py
+++ app.py
@@ -3,11 +3,27 @@ from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
+apps = []
+
 
 
 def addApp():
+
+     for widget in frame.winfo_children():
+        widget.destroy()
+
     filename= filedialog.askopenfilename(initialdir="/", title="Select File",
                                           filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame,text=app,bg="gray")
+        label.pack()
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
+
 canvas = tk.Canvas(root, height=500, width=500, bg="#263D42")
 canvas.pack()
 frame = tk.Frame(root,bg="white")
@@ -16,7 +32,10 @@ openFile = tk.Button(root, text="Open File", padx=15,
                      pady=10, fg="white", bg="maroon" ,command=addApp)
 openFile.pack()
 runApps = tk.Button(root, text="Run Apps", padx=15,
-                     pady=10, fg="white", bg="maroon")
+                     pady=10, fg="white", bg="maroon",command=runApps)
 
 runApps.pack()
 root.mainloop()
+with open('save.txt','w') as f:
+    for app in apps:
+        f.write(app + ',')
