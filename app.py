7b0d41e28546ba31f756079772bbb67bc7c00b5a
Quantum404 <sarah27166@gmail.com>
2020-11-26 08:17:33 +0530

    Removing empty spaces error

--- app.py
+++ app.py
@@ -1,41 +1,56 @@
+
 import tkinter as tk
 from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
 apps = []
+if os.path.isfile('save.txt'):
+    with open('save.txt','r') as f:
+        tempApps = f.read()
+        tempApps = tempApps.split(',')
+        apps = [x for x in tempApps if x.strip()]  
 
 
 
 def addApp():
 
-     for widget in frame.winfo_children():
+    for widget in frame.winfo_children():
         widget.destroy()
 
-    filename= filedialog.askopenfilename(initialdir="/", title="Select File",
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File",
                                           filetypes=(("executables", "*.exe"), ("all files", "*.*")))
     apps.append(filename)
     print(filename)
     for app in apps:
-        label = tk.Label(frame,text=app,bg="gray")
+        label = tk.Label(frame, text=app, bg="gray")
         label.pack()
+
 
 def runApps():
     for app in apps:
         os.startfile(app)
 
+
 canvas = tk.Canvas(root, height=500, width=500, bg="#263D42")
 canvas.pack()
-frame = tk.Frame(root,bg="white")
-frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 openFile = tk.Button(root, text="Open File", padx=15,
-                     pady=10, fg="white", bg="maroon" ,command=addApp)
+                     pady=10, fg="white", bg="maroon", command=addApp)
 openFile.pack()
 runApps = tk.Button(root, text="Run Apps", padx=15,
-                     pady=10, fg="white", bg="maroon",command=runApps)
+                    pady=10, fg="white", bg="maroon", command=runApps)
 
 runApps.pack()
+
+for app in apps:
+    label = tk.Label(frame,text=app)
+    label.pack()
+
+
 root.mainloop()
-with open('save.txt','w') as f:
+
+with open('save.txt', 'w') as f:
     for app in apps:
         f.write(app + ',')
